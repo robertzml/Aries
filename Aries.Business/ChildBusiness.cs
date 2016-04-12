0502a7b3817e62b0efb8d74fb6f9060e86ea8581
robertzml <robertzml@126.com>
2016-04-12 22:08:19 +0800

    Add ErrorCode class. Add AddChildWindow.
    Signed-off-by: robertzml <robertzml@126.com>

--- Aries.Business/ChildBusiness.cs
+++ Aries.Business/ChildBusiness.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aries.Data;
+using Aries.Model;
 
 namespace Aries.Business
 {
+    /// <summary>
+    /// 小孩业务类
+    /// </summary>
     public class ChildBusiness
     {
+        #region Field
+        /// <summary>
+        /// SQLite连接
+        /// </summary>
+        private SQLiteRepository sqlite;
+        #endregion //Field
+
+        #region Constructor
+        public ChildBusiness()
+        {
+            this.sqlite = new SQLiteRepository();
+        }
+        #endregion //Constructor
+
+        #region Method
+        /// <summary>
+        /// 添加小孩
+        /// </summary>
+        /// <param name="data">小孩对象</param>
+        /// <returns></returns>
+        public ErrorCode Add(Child data)
+        {
+            try
+            {
+                string sql = string.Format("INSERT Child(Name, Birth) VALUES('{0}', '{1}')", data.Name, data.Birth);
+                this.sqlite.ExecuteNonQuery(sql);
+                return ErrorCode.Success;
+            }
+            catch (Exception)
+            {
+                return ErrorCode.Exception;
+            }
+        }
+        #endregion //Method
     }
 }
--- Aries.UI/AddChildWindow.xaml.cs
+++ Aries.UI/AddChildWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Aries.UI
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// AddChildWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddChildWindow : Window
     {
-        public MainWindow()
+        public AddChildWindow()
         {
             InitializeComponent();
-        }
-
-        private void buttonAdd_Click(object sender, RoutedEventArgs e)
-        {
-            AddChildWindow window = new AddChildWindow();
-            window.ShowDialog();
         }
     }
 }
